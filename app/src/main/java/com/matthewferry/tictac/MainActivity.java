43f4d8504817fd102ac6076e3dfb4fab7ac82e19
Matthew Ferry <matthew.ferry007@gmail.com>
2019-03-30 12:48:18 +0100

    Middle and bottom row updated.

--- app/src/main/java/com/matthewferry/tictac/MainActivity.java
+++ app/src/main/java/com/matthewferry/tictac/MainActivity.java
@@ -14,11 +14,11 @@ import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //red=0, yellow=2, empty =2
+    //red=0, yellow=1, empty =2
     int i=1;
 
     int[] gameState ={2,2,2,2,2,2,2,2,2};
-    int [] [] winningPositions = {{0,1,2},{4,5,6},{7,8,9}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+    int [] [] winningPositions = {{0,1,2},{3,4,5},{6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
     boolean gameActive=true;
     int count_clicks=0;
     ImageView fireworks;
