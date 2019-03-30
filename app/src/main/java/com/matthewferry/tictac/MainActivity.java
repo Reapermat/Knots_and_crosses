01589b13b47a58785cee96f590ab9efd378515c0
Matthew Ferry <matthew.ferry007@gmail.com>
2019-03-30 11:42:13 +0100

    Middle and bottom row updated - didn't count as a win when got 3 in a row.

--- app/src/main/java/com/matthewferry/tictac/MainActivity.java
+++ app/src/main/java/com/matthewferry/tictac/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     int i=1;
 
     int[] gameState ={2,2,2,2,2,2,2,2,2};
-    int [] [] winningPositions = {{0,1,2},{0,1,2},{0,1,2}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+    int [] [] winningPositions = {{0,1,2},{4,5,6},{7,8,9}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
     boolean gameActive=true;
     int count_clicks=0;
     ImageView fireworks;
